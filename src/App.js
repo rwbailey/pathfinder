b3e57801661d557b2d26dc09b31207d0483e2377
Richard Bailey <richard@richardbailey.xyz>
2020-05-16 18:11:29 +0100

    Pathfinder

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PathfindingVisualiser from './PathfindingVisualiser/PathfindingVisualiser';
+
+function App() {
+  return (
+    <div className="App">
+      <PathfindingVisualiser></PathfindingVisualiser>
+    </div>
+  );
+}
+
+export default App;
